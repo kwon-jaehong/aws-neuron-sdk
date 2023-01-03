aaa3f9dab5b57f13e01e3ab137fc1ac6a80cc831
mrjaehong <mrjaehong54@gmail.com>
2023-01-03 05:11:41 +0000

    fix

--- inference_inf1.py
+++ inference_inf1.py
@@ -18,8 +18,8 @@ mnist_test = dsets.MNIST(root='MNIST_data/',
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1, padding='same')
-        self.conv2 = nn.Conv2d(32, 64, 3, 1, padding='same')
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
         self.dropout = nn.Dropout2d(0.25)
         # (입력 뉴런, 출력 뉴런)
         self.fc1 = nn.Linear(3136, 1000)    # 7 * 7 * 64 = 3136
