8022a76a97cf4b7ec083f14f4e4e716d502a1e89
mrjaehong <mrjaehong54@gmail.com>
2023-01-03 02:34:10 +0000

    fix

---
+++ mnist_train.py
@@ -0,0 +1,88 @@
+import torch
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+
+USE_CUDA = torch.cuda.is_available() # GPU를 사용가능하면 True, 아니라면 False를 리턴
+device = torch.device("cuda" if USE_CUDA else "cpu") # GPU 사용 가능하면 사용하고 아니면 CPU 사용
+print("다음 기기로 학습합니다:", device)
+training_epochs = 10
+batch_size = 256
+# MNIST dataset
+mnist_train = dsets.MNIST(root='MNIST_data/',
+                          train=True,
+                          transform=transforms.ToTensor(),
+                          download=True)
+
+mnist_test = dsets.MNIST(root='MNIST_data/',
+                         train=False,
+                         transform=transforms.ToTensor(),
+                         download=True)
+# dataset loader
+data_loader = DataLoader(dataset=mnist_train,
+                                          batch_size=batch_size, # 배치 크기는 100
+                                          shuffle=True,
+                                          drop_last=True)
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, padding='same')
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, padding='same')
+        self.dropout = nn.Dropout2d(0.25)
+        # (입력 뉴런, 출력 뉴런)
+        self.fc1 = nn.Linear(3136, 1000)    # 7 * 7 * 64 = 3136
+        self.fc2 = nn.Linear(1000, 10)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+model = CNN().to(device)
+criterion = nn.CrossEntropyLoss().to(device) # 내부적으로 소프트맥스 함수를 포함하고 있음.
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+for epoch in range(training_epochs): # 앞서 training_epochs의 값은 15로 지정함.
+    avg_cost = 0
+    total_batch = len(data_loader)
+
+    for X, Y in data_loader:
+        X = X.to(device)
+        Y = Y.to(device)
+
+        optimizer.zero_grad()
+        hypothesis = model(X)
+        cost = criterion(hypothesis, Y)
+        cost.backward()
+        optimizer.step()
+
+        avg_cost += cost / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+torch.save(model.state_dict(),"./mnist_base.pth")
+print('Learning finished')
+
+
+# test_loader = DataLoader(dataset=mnist_test,
+#                                            batch_size = batch_size, shuffle = True)
+# model.eval()    # 평가시에는 dropout이 OFF 된다.
+# correct = 0
+# for data, target in test_loader:
+#     data = data.to(device)
+#     target = target.to(device)
+#     output = model(data)
+#     prediction = output.data.max(1)[1]
+#     correct += prediction.eq(target.data).sum()
+# print('Test set Accuracy : {:.2f}%'.format(100. * correct / len(test_loader.dataset)))
