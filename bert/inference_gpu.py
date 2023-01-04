895ec3c5462cd58064174b528b44e87c9a0c3e93
mrjaehong <mrjaehong54@gmail.com>
2023-01-04 07:05:09 +0000

    fix

--- bert/inference_gpu.py
+++ bert/inference_gpu.py
@@ -24,12 +24,21 @@ pytorch_total_params = sum(p.numel() for p in model.parameters())
 print(f"모델 파라미터 갯수 {pytorch_total_params}")
 
 
+input_ids = paraphrase['input_ids']
+token_type_ids = paraphrase['token_type_ids']
+attention_mask = paraphrase['attention_mask']
+
+input_ids = input_ids.to(device=device)
+token_type_ids = token_type_ids.to(device=device)
+attention_mask = attention_mask.to(device=device)
+
+
 latency = []
 # 1장씩 100번 넣음
 num_infers = 100
 for _ in range(num_infers):
     delta_start = time()
-    paraphrase_classification_logits = model(**paraphrase)[0]
+    paraphrase_classification_logits = model(input_ids,token_type_ids,attention_mask)[0]
     delta = time() - delta_start
     latency.append(delta)
 ## 처리 속도 평균
