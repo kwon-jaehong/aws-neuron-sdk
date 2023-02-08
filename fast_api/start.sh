56c5b74c7a980d21e8284ab44dabfec2f6d5f251
mrjaehong <mrjaehong54@gmail.com>
2023-02-08 01:43:10 +0000

    fix

--- fast_api/start.sh
+++ fast_api/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-neuron-monitor -c ./monitor.conf |python3.7 ./mod-neuron-monitor-prometheus.py --port 9000 &
+neuron-monitor -c ./monitor.conf |python3 ./mod-neuron-monitor-prometheus.py --port 9000 &
 python myapp.py
